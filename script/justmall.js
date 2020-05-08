8ea18f06dc24f8e6c90a07f9de2b4e8cef25781e
leeyuri872 <uleeuzz@gmail.com>
2020-05-08 11:52:24 +0900

    tab기능 누락 추가

--- script/justmall.js
+++ script/justmall.js
@@ -2,18 +2,25 @@ $(function(){
     tabs();  //tabs 기능 호출(=실행)
     popup(); // popup 기능 호출   
 });
+
 function tabs(){
-     //공지사항 누르면 공지사항 보여주고(갤러리는 감춰라) 갤러리누르면 그 반대
-     $(".board_title li").click(function(){//클릭하면 다음을 실행해
-        //클릭한 것이 .on을 갖고있는지 그럼 무엇을 시킬것인지
-        if(!$(this).hasClass("on")) {//on이 없다면!!
-            //즉 #gallery를 선택한 것을 의미함
-            $("#gallery").css("display","block");
-        } else {
-            $("#gallery").css("display","none");
+    $(".board_title li").click(function(){
+        if($(this).hasClass("on")){
+            //on을 갖고 있다면?
+            //아무것도 할게 없어요..
+        } else {//아니면
+            $(this).siblings().removeClass("on");
+            $(this).addClass("on");
+            $("#gallery").toggleClass("hide");
+            
+            //on을 갖고 있지 않다면?
+            //기존에 누군가 갖고 있던 on을 삭제하고,
+            //지금 클릭한 (on에 없던 것에) 추가해
         }
-    });    
+    });
 }
+
+
 function popup(){
     popup_open();
     popup_close();
